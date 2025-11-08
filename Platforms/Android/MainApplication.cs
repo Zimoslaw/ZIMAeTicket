9463257a34e990c81815d038ce678ba451a2ca8b
Jakub <35482785+Zimoslaw@users.noreply.github.com>
2025-11-08 02:09:09 +0100

    Minor changes in project properties for better android devices support

--- Platforms/Android/MainApplication.cs
+++ Platforms/Android/MainApplication.cs
@@ -3,7 +3,7 @@ using Android.Runtime;
 
 namespace ZIMAeTicket
 {
-    [Application]
+    [Application (UsesCleartextTraffic = true)]
     public class MainApplication : MauiApplication
     {
         public MainApplication(IntPtr handle, JniHandleOwnership ownership)
