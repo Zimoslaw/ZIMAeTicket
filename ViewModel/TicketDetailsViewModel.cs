256e6aeb96d5a00a3e1b17ce8813b81682ca474c
Jakub <35482785+Zimoslaw@users.noreply.github.com>
2025-11-18 22:26:23 +0100

    Resending ticket
    + Button for resending a ticket to customer
    + Entry for new e-mail address to send ticket to

--- ViewModel/TicketDetailsViewModel.cs
+++ ViewModel/TicketDetailsViewModel.cs
@@ -1,4 +1,6 @@
-﻿using ZIMAeTicket.Services;
+﻿using System.IO.Pipelines;
+using System.Text.RegularExpressions;
+using ZIMAeTicket.Services;
 
 namespace ZIMAeTicket.ViewModel
 {
@@ -8,7 +10,18 @@ namespace ZIMAeTicket.ViewModel
         [ObservableProperty]
         Ticket ticket;
 
+#if WINDOWS
+        [ObservableProperty]
+        string resendEmailAddress;
+
+        [GeneratedRegex(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$")]
+        private static partial Regex EmailAddressRegex();
+
+#endif
+
         TicketService ticketService;
+        MailingService mailingService;
+        SoteshopService soteshopService;
 
         [ObservableProperty]
         [NotifyPropertyChangedFor(nameof(IsNotUsed))]
@@ -16,15 +29,25 @@ namespace ZIMAeTicket.ViewModel
 
         public bool IsNotUsed => !IsUsed;
 
-        public TicketDetailsViewModel(TicketService ticketService)
+#if WINDOWS
+        public TicketDetailsViewModel(TicketService ticketService, MailingService mailingService, SoteshopService soteshopService)
         {
             Title = $"Szczegóły biletu";
             this.ticketService = ticketService;
+            this.mailingService = mailingService;
+            this.soteshopService = soteshopService;
+        }
+#endif
+        public TicketDetailsViewModel(TicketService ticketService, MailingService mailingService)
+        {
+            Title = $"Szczegóły biletu";
+            this.ticketService = ticketService;
+            this.mailingService = mailingService;
         }
 
         public async Task ChangeUsedProperty()
         {
-            IsUsed = ticket.Used;
+            IsUsed = Ticket.Used;
         }
 
         [RelayCommand]
@@ -56,5 +79,74 @@ namespace ZIMAeTicket.ViewModel
                 IsBusy = false;
             }
         }
+
+#if WINDOWS
+        [RelayCommand]
+        async Task ResendTicket()
+        {
+            ResendEmailAddress = Ticket.OrderEmail;
+
+            if (string.IsNullOrEmpty(ResendEmailAddress) || !EmailAddressRegex().IsMatch(ResendEmailAddress))
+            {
+                await Shell.Current.DisplayAlert("Wysyłanie biletu", "Wprowadzony adres nie jest poprawnym adresem e-mail", "OK");
+                return;
+            }
+
+            bool initResult = mailingService.InitSMTPConnection();
+
+            if (!initResult)
+            {
+                await Shell.Current.DisplayAlert("Błąd", $"Błąd inicjalizacji z serwerem poczty: {mailingService.StatusMessage}", "OK");
+                return;
+            }
+
+            try
+            {
+                IsBusy = true;
+
+                string dateOfEmail = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+
+                TicketGroup ticketGroup = await ticketService.GetTicketGroupById(Ticket.TicketGroupId);
+
+                mailingService.InitMessage(Ticket.OrderId, dateOfEmail, Ticket.DateOfOrder, ResendEmailAddress, Ticket.Buyer, ticketGroup.Name);
+
+                bool attachResult = await mailingService.AttatchQRCodeToMessage(Ticket.Hash);
+                if (!attachResult)
+                {
+                    throw new Exception(mailingService.StatusMessage);
+                }
+
+                bool sendResult = await mailingService.SendMail();
+                if (!sendResult)
+                {
+                    throw new Exception(mailingService.StatusMessage);
+                }
+
+                Ticket.DateOfEmail = dateOfEmail;
+
+                // Putting sent ticket into db table with ready to download and scan tickets
+                bool putResult = await soteshopService.PutTicketIntoRemoteDatabase(Ticket);
+
+                if (!putResult)
+                {
+                    throw new Exception(soteshopService.StatusMessage);
+                }
+
+                await ticketService.UpdateTicket(Ticket);
+            }
+            catch (Exception ex)
+            {
+                await Shell.Current.DisplayAlert("Błąd", $"Nie udało się wysłać biletu: {ex.Message}", "OK");
+            }
+            finally
+            {
+                mailingService.CloseSMTPConnection();
+
+                IsBusy = false;
+
+                await Shell.Current.DisplayAlert("Wysyłanie biletu", $"Bilet wysłany ponownie do {ResendEmailAddress}", "OK");
+            }
+        }
+#endif
     }
 }
