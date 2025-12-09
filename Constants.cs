53672105bbef4362260cd7dd465724d0be69ab45
Jakub <35482785+Zimoslaw@users.noreply.github.com>
2025-12-10 00:50:36 +0100

    Update Constants.cs

--- Constants.cs
+++ Constants.cs
@@ -5,6 +5,6 @@
         public const string DatabaseFilename = "ZIMAeTicket.db3";
 
         public static string DatabasePath =>
-            Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename);
+            Path.Combine(FileSystem.Current.AppDataDirectory, DatabaseFilename);
     }
 }
